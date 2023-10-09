2963447be0579ab132a4b49d1eb136e0391018ff
Fajarsyah11 <4kenzo51@gmail.com>
2023-10-09 20:21:30 +0700

    Pembuatan Halaman depan dan About rentalbuku.net

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Fajar
+        saya tinggal di karet tengsin olahraga yang saya sukai adalah tenis meja";
+    }
+}
